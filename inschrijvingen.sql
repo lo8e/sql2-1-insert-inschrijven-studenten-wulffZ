392632497fb446e9d053cf8ca22d8afc6903ce81
Matthijs <1042400@mborijnland.nl>
2019-05-08 09:50:39 +0200

    Inserted 4 velden in studenten
    yeet

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,8 @@
+
 USE lo8e_sql2;
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+INSERT INTO studenten (ov_nummer, plaats, voornaam, achternaam)
 VALUES 
-('lo9e-amo1', 'vlt03', '2019', 'amo'),
-('lo9e-amo2', 'bkr02', '2019', 'amo'),
-('lo9e-amo3', 'akr02', '2019', 'amo');
+('90009', '85', 'Matthijs', 'van Zetten');
+
+
 
