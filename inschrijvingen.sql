964a98e4df64f56b982b31aadb0ea54b23ab27f5
Matthijs <1042400@mborijnland.nl>
2019-05-08 09:44:35 +0200

    Commited insert
    inserted

---
+++ inschrijvingen.sql
@@ -0,0 +1,7 @@
+USE lo8e_sql2;
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES 
+('lo9e-amo1', 'vlt03', '2019', 'amo'),
+('lo9e-amo2', 'bkr02', '2019', 'amo'),
+('lo9e-amo3', 'akr02', '2019', 'amo');
+
