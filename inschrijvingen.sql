79ff7b5afef5273d2cc6cba79528f391d3bd9a86
Matthijs <1042400@mborijnland.nl>
2019-05-10 12:10:04 +0200

    Geprobeerd records toe te voegen

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,7 @@
-INSERT INTO studenten (ov_nummer, plaats, voornaam, achternaam)
-VALUES 
-('90001', '90002', '90003', '90004');
+USE lo8e_sql2;
 
-
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+    (90001,'lo9e-amo1'),
+	(90002,'lo9e-amo2'),
+    (90003,'lo9e-amo3'),
+    (90004,'lo9e-amo3');
