a7c0f86e4350afbf6e5971a44d22fa1b749443c8
Matthijs <1042400@mborijnland.nl>
2019-05-10 12:33:03 +0200

    Opdracht 6 klaar en werkend
    yeetyeetyeet eindelijk klaar

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,9 @@
-UPDATE studenten
-SET actief = false
-WHERE inschrijvings_datum <= "1-1-2017";
-hij staat in safe mode, kan niet editen
+SELECT concat(studenten.voornaam,' ', studenten.tussenvoegsel,' ', studenten.achternaam) AS Naam, klassen.cohort , klassen.klas_code, klassen_studenten.ov_nummer, concat(docenten.voorletters, ' ', docenten.tussenvoegsel, ' ', docenten.achternaam) AS 'slb-er' FROM studenten
+INNER JOIN klassen_studenten 
+ON klassen_studenten.ov_nummer = studenten.ov_nummer
+INNER JOIN klassen
+ON klassen.klas_code = klassen_studenten.klas_code
+INNER JOIN docenten
+ON klassen.slb_code = docenten.docent_code
+WHERE studenten.actief = true
+ORDER BY  klassen.klas_code ASC, studenten.achternaam ASC;
