221c45d196d88e13e2362296714ff1a653681a16
Matthijs <1042400@mborijnland.nl>
2019-05-10 12:15:36 +0200

    Opdracht 4

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,3 @@
-USE lo8e_sql2;
-
-INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
-    (90001,'lo9e-amo1'),
-	(90002,'lo9e-amo2'),
-    (90003,'lo9e-amo3'),
-    (90004,'lo9e-amo3');
+UPDATE studenten
+SET geboortedatum = "1964-09-31"
+WHERE ov_nummer = 90001;
